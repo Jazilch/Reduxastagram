6a687b18f9b5236dfd0cabcf4d8e240170bdc517
Jazilch <jzilch@hubspot.com>
2018-04-17 13:53:27 -0400

    Initial Commit

---
+++ client/reducers/posts.js
@@ -0,0 +1,22 @@
+// A reducer take in two things:
+
+// 1. the action (info about what happened)
+// 2. copy of current state
+
+function posts(state = [], action) {
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+    // return the updated state
+    const i = action.index;
+    return [
+      ...state.slice(0, i),
+      {...state[i], likes: state[i].likes + 1},
+      ...state.slice(i + 1),
+    ]
+    default:
+     return state;
+  }
+  return state;
+}
+
+export default posts;
---
+++ client/reduxstagram.js
@@ -0,0 +1,29 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+// Import Components 
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+//import react router
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
